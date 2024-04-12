7bafdf4376839a83c4a177b9881dac34e534cb70
anthonyafgx <89319333+anthonyafgx@users.noreply.github.com>
2024-04-12 00:59:05 -0400

    Environment Preparation and Adjustments

--- dns/cloudflare.py
+++ dns/cloudflare.py
@@ -1,6 +1,17 @@
 from dns.interface import DNSInterface
 
+import os
+
 class CloudflareDNS(DNSInterface):
+    _zone_id: (str | None)
+
+    def __init__(self, api_key:str, domain_name:str):
+        # LOAD NS SERVICE ENV VARIABLES
+        self._zone_id = os.getenv("CLOUDFLARE_ZONE_ID")
+        if (self._zone_id == None or self._zone_id == ""):
+            raise ValueError("CLOUDFLARE_ZONE_ID NOT DEFINED")
+        return super().__init__(api_key=api_key, domain_name=domain_name)
+
     def update_dns_record(self, new_ip: str) -> bool:
         
         
--- main.py
+++ main.py
@@ -1,24 +1,34 @@
 import os
+from dotenv import load_dotenv
+import time
 
 from dns.interface import DNSInterface
 from dns.cloudflare import CloudflareDNS
+from ip.interface import IPInterface
+from ip.ipify import Ipify
 
-def main(dns: DNSInterface):
-    pass
+def main(dns: DNSInterface, ip: IPInterface):
+    update_interval: float = float(os.getenv(key="UPDATE_INTERVAL_SECONDS",default=60.0))
+    last_ip: str = ""
+    while True:
+        new_ip: str = ip.scan_ipv4()
+        if (last_ip != new_ip):
+            dns.update_dns_record(new_ip=new_ip)
+        time.sleep(update_interval)
 
 if __name__ == "__main__":
+    load_dotenv()
+    
     api_key: (str | None) = os.getenv('API_KEY')
     domain_name: (str | None) = os.getenv('DOMAIN_NAME')
     
     # GENERAL ENV VARIABLES
-    if (api_key == None): 
+    if (api_key == None or api_key == ""): 
         raise ValueError("API_KEY not defined")
 
-    if (domain_name == None):
+    if (domain_name == None or domain_name == ""):
         raise ValueError("DOMAIN_NAME not deinfed")
 
-    # CLOUDFLARE ESPECIFIC ENV VARIABLES
-    cf_zone_id: (str | None) = os.getenv('CLOUDFLARE_ZONE_ID')
-
     dns = CloudflareDNS(api_key=api_key, domain_name=domain_name)
-    main(dns=dns)
+    ip = Ipify()
+    main(dns=dns, ip=ip)
