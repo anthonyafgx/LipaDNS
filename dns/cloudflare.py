da389ef6ec048e6e2af618c050a73630d5f600de
anthonyafgx <89319333+anthonyafgx@users.noreply.github.com>
2024-04-12 01:18:01 -0400

    Cloudflare API PT.1

--- dns/cloudflare.py
+++ dns/cloudflare.py
@@ -1,6 +1,7 @@
 from dns.interface import DNSInterface
 
 import os
+import requests
 
 class CloudflareDNS(DNSInterface):
     _zone_id: (str | None)
@@ -13,6 +14,20 @@ class CloudflareDNS(DNSInterface):
         return super().__init__(api_key=api_key, domain_name=domain_name)
 
     def update_dns_record(self, new_ip: str) -> bool:
+        # Check if domain exists
+        url: str = f"https://api.cloudflare.com/client/v4/zones/{self._zone_id}/dns_records"
         
-        
+        headers: dict[str,str] = {
+            "Content-Type": "application/json",
+            "X-Auth-Email": ""
+        }
+
+        parameters: dict[str,str] = {
+            "name": self.domain_name
+        }
+
+        response = requests.request("GET", url=url, headers=headers, params=parameters)
+
+        # if (response.json)
+
         return True
